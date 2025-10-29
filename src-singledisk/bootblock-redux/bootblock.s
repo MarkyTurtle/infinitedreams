168c438a73c9de58891bdc89cd52564ec166fed5
mark <markgalsworthy@hotmail.com>
2025-10-29 14:29:27 +0000

    small code organisation and commenting.

--- src-singledisk/bootblock-redux/bootblock.s
+++ src-singledisk/bootblock-redux/bootblock.s
@@ -2,17 +2,14 @@
 ; INFINITE DREAMS - 1 disked - music disk
 ; (c)1992-2025 - The Lunatics UK
 ;
-; Basic MFM Boot loader to load the Zx0 Compressed 'demo' file from the Disk.
-; The executable demo is decompressd to $2000 to $1c000 in memory (with slack space for exe growth)
-;
-; The code manages to fill every available byte in the boot block 1024 bytes.
-; It could be made shorter but I wanted to make it readable enough.
+; - Loads the disk file table.
+; - Loads and displays the Title Picture from the disk.
+; - Loads and executed the main program 'demo' from the disk.
 ;
 
                     section     bootloader,code
                     incdir      "include/"
                     include     "hw.i"
-
 
 
 TESTBOOT SET 1         ; Comment this to remove 'testboot' and build boot block for actual disk.
@@ -70,7 +67,7 @@ relocate_bootloader:    lea     boot_loader(pc),a0
             ; ------------- relocated boot loader $00001000 -----------
             ;----------------------------------------------------------
 boot_loader             lea     STACK_ADDRESS,A7
-                        bsr     set_interrupt_vectors
+                        ;bsr     set_interrupt_vectors
                         bsr     load_file_table
                         bsr     load_title_picture
                         bsr     display_title_picture
@@ -82,13 +79,13 @@ boot_loader             lea     STACK_ADDRESS,A7
                         jmp     DEMO_START_ADDRESS
 
 
-            ; -------------- set interrupt vectors ----------------
-set_interrupt_vectors   move.w  #$6,d7
-                        lea     $64.w,a0
-                        lea     do_nothing_handler(pc),a1
-.set_vector_loop        move.l  a1,(a0)+
-                        dbf     d7,.set_vector_loop
-                        rts
+;            ; -------------- set interrupt vectors ----------------
+;set_interrupt_vectors   move.w  #$6,d7
+;                        lea     $64.w,a0
+;                        lea     do_nothing_handler(pc),a1
+;.set_vector_loop        move.l  a1,(a0)+
+;                        dbf     d7,.set_vector_loop
+;                        rts
 
 
             ; ------------------ load file table -------------------
@@ -171,8 +168,7 @@ display_title_picture
             ; ------------------ fade out title picture ---------------------
             ; overwrite image palette with black colour value for
             ; 32 colour entries. Then fall through to the fade in function
-fade_out_title_picture
-                        move.w  #32-1,d7
+fade_out_title_picture  move.w  #32-1,d7
                         lea     TPIC_START_ADDRESS,a0       ; set 32 colour palette to black 
 .loop                   move.w  #$0000,(a0)+
                         dbf     d7,.loop
@@ -180,17 +176,14 @@ fade_out_title_picture
             ; ------------------- fade in title picture --------------------
             ; fade in copper list colours to 32 colour entries stored at
             ; the start of the title picture image data.
-fade_in_title_picture  ; fade in title pic
-                        move.w  #16,D7
-.raster_wait_1          bsr     raster_wait
-
+fade_in_title_picture   move.w  #16-1,D7                    ; fade in 16 steps (16 colour levels per r,g,b)
+.do_fade                bsr     raster_wait                 ; one fade per frame
                         move.l  d7,-(a7)
-                        lea     TPIC_START_ADDRESS,a0 
-                        lea     TPIC_COPPER_ADDR,a1
-                        bsr     fade_copper_colours
-                        move.l  (a7)+,d7
-
-                        dbf     d7,.raster_wait_1
+                        lea     TPIC_START_ADDRESS,a0       ; target colour palette ptr
+                        lea     TPIC_COPPER_ADDR,a1         ; copper colour registers ptr
+                        bsr     fade_copper_colours         ; do fade
+                        move.l  (a7)+,d7    
+                        dbf     d7,.do_fade
                         rts
 
 
@@ -217,37 +210,40 @@ do_nothing_handler:     rte
 
             ; --------------- load and decompress file -------------------
             ; d0 = fileid
-            ; a0 = load address
-            ; a1 = decompress address
-load_file               movem.l d0-d7/a0-a6,-(a7)
+            ; a0 = load address ptr
+            ; a1 = decompress address ptr
+load_file               movem.l d0-d7/a0-a6,-(a7)               ; save all registers
+
                         move.w  #27-1,d7                        ; size of file table (26 entries)
-                        lea     FILE_TABLE_ADDR,a3
+                        lea     FILE_TABLE_ADDR,a3              ; disk file table, see infinitedreams.adf.filetable.txt
 
 .find_file          ; find file id (d0) in file table
-                        cmp.l   (a3),d0
-                        beq     .load_file
-                        lea     $10(a3),a3
-                        dbra    d7,.find_file
+                        cmp.l   (a3),d0                         ; have we found the fileid?
+                        beq     .load_file                      ; ...yes, then load it.
+                        lea     $10(a3),a3                      ; try next file table entry
+                        dbra    d7,.find_file                   ; check next table entry.
 
 .load_error         ; load error
-                        move.w  #$f00,$dff180
-                        bra.s   .load_error
+                        move.w  #$f00,$dff180                   ; turn screen red
+                        bra.s   .load_error                     ; loop forever.
 
-.load_file          ; load file from disk to (a0)
+.load_file          ; load file from disk to address a0
                         move.l  $4(a3),d0                       ; disk byte offset
                         move.l  $c(a3),d1                       ; disk file length
                         moveq   #0,d2                           ; drive 0
                         lea     MFM_BUFFER,a1
                         bsr     byteloader
-                        tst.l   d0
+                        tst.l   d0                              ; test for load error
                         bne.s   .load_error                     ; some kinda disk error.
 
-                        movem.l (a7),d0-d7/a0-a6
+                        movem.l (a7),d0-d7/a0-a6                ; restore saved registers
 
-                    ; decompress file to (a1)
-                        bsr     zx0_decompress
+                    ; decompress file to address a1
+                    ; a0 = load address ptr
+                    ; a1 = decompress address ptr
+                        bsr     zx0_decompress                  ; decompress file
 
-                        movem.l (a7)+,d0-d7/a0-a6
+                        movem.l (a7)+,d0-d7/a0-a6               ; restore saved registers again...
                         rts
 
 
@@ -258,32 +254,32 @@ load_file               movem.l d0-d7/a0-a6,-(a7)
 fade_copper_colours
                     move.w  #32-1,d7
 .fade_loop
-                    move.w  (a0)+,d0            ; colour to fade towards
-                    move.w  2(a1),d2            ; current colour from copper list
-                    move.w  #$000f,d4           ; r,g,b mask value
-                    move.w  #$0001,d5           ; r,g,b increment/decrement value 
-                    move.w  #3-1,d6             ; 3 colour components to fade in r,g,b
+                    move.w  (a0)+,d0                            ; palette colour to fade towards
+                    move.w  2(a1),d2                            ; current colour from copper list
+                    move.w  #$000f,d4                           ; initial r,g,b mask value
+                    move.w  #$0001,d5                           ; initial r,g,b increment/decrement value 
+                    move.w  #3-1,d6                             ; 3 colour components to fade in r,g,b
 .fade_component
-                    move.w  d0,d1               ; d1 = copy of fade towards colour
-                    move.w  d2,d3               ; d3 = copy of current colour
-                    and.w   d4,d1               ; mask r,g, or b component
-                    and.w   d4,d3               ; mask r,g, ot b component
-                    cmp.w   d1,d3               ; is current colour component equal to, more than or less than the target colour
-                    beq.s   .next_component     ; is equal so no change required
+                    move.w  d0,d1                               ; d1 = working copy of palette colour
+                    move.w  d2,d3                               ; d3 = working copy of current colour
+                    and.w   d4,d1                               ; mask r,g, or b component
+                    and.w   d4,d3                               ; mask r,g, ot b component
+                    cmp.w   d1,d3                               ; is current colour component equal to, more than or less than the target colour
+                    beq.s   .next_component                     ; is equal so no change required
                     bgt.s   .fade_down
-.fade_up
-                    add.w   d5,d2               ; fade colour component up (make r,g, or b brighter)
+.fade_up            ; make r,g, or b brighter
+                    add.w   d5,d2                               ; fade colour component up (make r,g, or b brighter)
                     bra     .next_component
-.fade_down
-                    sub.w   d5,d2               ; fade colour component down (make r,g, or b darker)
+.fade_down          ; make r,g, or b darker
+                    sub.w   d5,d2                               ; fade colour component down (make r,g, or b darker)
 .next_component
-                    rol.w   #4,d4               ; rotate mask to next r,g,b component
-                    rol.w   #4,d5               ; rotate inecrement/decrement value to next r,g,b component
-                    dbf.w   d6,.fade_component  ; do next r,g or b component
+                    rol.w   #4,d4                               ; rotate mask to next r,g,b component
+                    rol.w   #4,d5                               ; rotate inecrement/decrement value to next r,g,b component
+                    dbf.w   d6,.fade_component                  ; do next r,g or b component
 
-.update_color       move.w  d2,2(a1)            ; store faded colour bak in copper list
-                    addq.l  #4,a1               ; update ptr to next copper instruction
-                    dbf     d7,.fade_loop       ; fade next colour in copper list
+.update_color       move.w  d2,2(a1)                            ; store faded colour back in copper list
+                    addq.l  #4,a1                               ; update ptr to next copper instruction
+                    dbf     d7,.fade_loop                       ; fade next colour in copper list
 
                     rts     
 
